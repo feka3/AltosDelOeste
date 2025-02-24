c026bf0447629681167420625a602de645e71b6e
feka3 <farriola14@gmail.com>
2025-02-24 07:56:37 -0300

    change styles

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Aquí puedes agregar la lógica JavaScript que quieras, por ejemplo:
 
-document.querySelectorAll('.lot').forEach(lot => {
+document.querySelectorAll('.cls-7').forEach(lot => {
     lot.addEventListener('click', (e) => {
         const lotId = e.target.id; // Para obtener el id del lote clickeado
         alert(`Haz clic en el lote con ID: ${lotId}`);
